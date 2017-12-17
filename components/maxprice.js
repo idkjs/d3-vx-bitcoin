783efd3c0eb81df0db8dc8544a0ad218aac4b8d6
Alain Armand <aarmand.inbox@gmail.com>
2017-12-17 12:21:44 +0100

    <MaxPrice strokeDasharray="4.4", strokeOpacity="0.4"

--- components/maxprice.js
+++ components/maxprice.js
@@ -4,8 +4,16 @@ export default ({ data, label, yScale, xScale, yText, x, y }) => {
   return (
     // <g> = group tag
     <g>
-      <LinePath data={data} yScale={yScale} xScale={xScale} x={x} y={y} />
-      <text fill="white" dy="1.3em" dx="1em" y={yText} fontSize="12">
+      <LinePath
+        data={data}
+        yScale={yScale}
+        xScale={xScale}
+        x={x}
+        y={y}
+        strokeDasharray="4.4"
+        strokeOpacity="0.4"
+      />
+      <text fill="#6086d6" dy="1.3em" dx="1em" y={yText} fontSize="12">
         {label}
       </text>
     </g>
